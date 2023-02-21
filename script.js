87c3613b2afaf7fd2bcdb731e5408e733c4c8294
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-21 14:56:44 +0300

    createBoard function

---
+++ script.js
@@ -0,0 +1,16 @@
+
+createBoard(16);
+
+function createBoard(size){
+    let board = document.querySelector(".board")
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numberOfDivs = size * size;
+
+    for (let i = 0; i < numberOfDivs; i++) {
+        let div = document.createElement("div")
+        div.style.backgroundColor = "yellow"
+        board.insertAdjacentElement("beforeend", div)
+    }
+}
